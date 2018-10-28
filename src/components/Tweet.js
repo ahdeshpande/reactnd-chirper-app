5eb1de7a3ac904e110cff957e6b5d09594464534
ahdeshpande <deshpande.abhiraj@gmail.com>
2018-10-27 22:21:18 -0400

    feat: Toggle tweet like

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -6,6 +6,7 @@ import {
     TiHeartOutline,
     TiHeartFullOutline
 } from "react-icons/ti/index";
+import {handleToggleTweet} from "../actions/tweets";
 
 class Tweet extends React.Component {
 
@@ -17,7 +18,13 @@ class Tweet extends React.Component {
     handleLike = e => {
         e.preventDefault();
 
-        //TODO: Handle like tweet
+        const {dispatch, tweet, authedUser} = this.props;
+
+        dispatch(handleToggleTweet({
+            id: tweet.id,
+            hasLiked: tweet.hasLiked,
+            authedUser,
+        }))
     };
 
     render() {
