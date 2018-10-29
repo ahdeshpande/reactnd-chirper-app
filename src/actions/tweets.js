96cca22e7e27a98b457e73a5e402d6682de60890
ahdeshpande <deshpande.abhiraj@gmail.com>
2018-10-28 22:36:12 -0400

    feat: New tweet

--- src/actions/tweets.js
+++ src/actions/tweets.js
@@ -1,12 +1,21 @@
-import {saveLikeToggle} from "../utils/api";
+import {saveLikeToggle, saveTweet} from "../utils/api";
+import {hideLoading, showLoading} from "react-redux-loading";
 
 export const RECEIVE_TWEETS = 'RECEIVE_TWEETS';
 export const TOGGLE_TWEET = 'TOGGLE_TWEET';
+export const ADD_TWEET = 'ADD_TWEET';
 
 export function receiveTweets(tweets) {
     return {
         type: RECEIVE_TWEETS,
         tweets,
+    }
+}
+
+function addTweet(tweet) {
+    return {
+        type: ADD_TWEET,
+        tweet,
     }
 }
 
@@ -29,5 +38,23 @@ export function handleToggleTweet(info) {
                 dispatch(toggleTweet(info));
                 alert('There was an error liking the tweet. Try again.');
             })
+    }
+}
+
+export function handleAddTweet(text, replyingTo) {
+    return (dispatch, getState) => {
+
+        const {authedUser} = getState();
+        dispatch(showLoading());
+
+        return saveTweet({
+            text,
+            author: authedUser,
+            replyingTo,
+        })
+            .then((tweet) => {
+                dispatch(addTweet(tweet));
+            })
+            .then(() => dispatch(hideLoading()));
     }
 }
